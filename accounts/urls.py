bfae8c0f65a8dff9fa86a60fd0cd9b215eebe59e
Midhun Kumar <midhun.kumar@flavorcloud.com>
2023-02-25 18:33:47 +0530

    'accounts'

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('logout',views.logoutMethod,name="logout"),
     path('chat',views.chatbotPage,name="chatbotPage"),
     path('chatquery',views.chatbotquery,name="chatbotquery"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(),name="reset_password"),
+    path('password_reset_sent/', auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
      ]
--- chatbot/settings.py
+++ chatbot/settings.py
@@ -118,6 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#email
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='chatbotsystemnoreply@gmail.com'
+EMAIL_HOST_PASSWORD='55555@pink'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
